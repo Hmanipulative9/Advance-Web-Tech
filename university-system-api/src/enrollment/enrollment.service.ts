9c641be284fe148ab456b681a5fcd7019bb4a18d
Hmanipulative9 <haass9b@gmail.com>
2026-07-03 21:35:11 +0600

    enrollment services added

--- university-system-api/src/enrollment/enrollment.service.ts
+++ university-system-api/src/enrollment/enrollment.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CourseService } from '../course/course.service';
 
 @Injectable()
-export class EnrollmentService {}
+export class EnrollmentService {
+    constructor(private readonly courseService: CourseService) { }
+
+    enrollStudent(studentName: string, courseId: string) {
+        // Call the CourseService to retrieve course information
+        const course = this.courseService.getCourseById(courseId);
+        return {
+            message: 'Student enrolled successfully',
+            student: studentName,
+            course: course,
+        };
+    }
+
+    getEnrollments() {
+        return { message: 'All enrollments fetched', data: [] };
+    }
+}
